b6428515f472b9898b302de537e5d9184237d5b8
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-22 14:08:43 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 echo 'Coucou'
 read -p 'Dite moi un truc : ' reponse
 echo 'tu a dit : ' $reponse 
+
+if [ $reponse = "Coucou" ]; then
+echo 'Tu est poli merci'
+fi
+
+if [ $reponse = "Boucle" ]; then
+while [ $reponse = "Boucle" ]; do
+echo "JE BOUCLE"
+done
+fi
